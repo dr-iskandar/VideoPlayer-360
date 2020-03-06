f5d374f409227d57f5cafc630e2774d62f5fd2a7
dr-iskandar <drachmat.iskandar@gmail.com>
2020-03-06 16:03:37 +0700

    Initial commit

---
+++ Assets/Scripts/ViewController.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class ViewController : MonoBehaviour
+{
+    public GameObject[] allState;
+    public Animator transition;
+    public Texture2D cursorHover;
+    public CursorMode cursorMode = CursorMode.Auto;
+    public Vector2 hotSpot = Vector2.zero;
+    public TMP_Text title;
+
+    public void changeState(GameObject nextView)
+    {
+        StartCoroutine(ImageTransition(nextView));
+    }
+
+    public void MouseEnter()
+    {
+        Cursor.SetCursor(cursorHover, hotSpot, cursorMode);
+    }
+
+    public void MouseExit()
+    {
+        Cursor.SetCursor(null, Vector2.zero, cursorMode);
+    }
+
+    IEnumerator ImageTransition(GameObject nextState)
+    {
+
+        transition.SetTrigger("in");
+        yield return new WaitForSeconds(0.6f);
+        Cursor.SetCursor(null, Vector2.zero, cursorMode);
+        for (int i = 0; i < allState.Length; i++)
+        {
+            allState[i].SetActive(false);
+        }
+        nextState.SetActive(true);
+    }
+
+    public void changeTitle(TMP_Text Title)
+    {
+        title.text = Title.text;
+    }
+}
---
+++ Assets/VideoController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+
+public class VideoController : MonoBehaviour
+{
+    public GameObject[] vps;
+
+    public void changeVideo(GameObject myGameObject)
+    {
+        for (int i = 0; i < vps.Length; i++)
+        {
+            vps[i].GetComponent<VideoPlayer>().Stop();
+            vps[i].SetActive(false);
+            myGameObject.SetActive(true);
+            myGameObject.GetComponent<VideoPlayer>().Play();
+        }
+    }
+}
